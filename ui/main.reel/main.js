337b526502f112b08d5793ed8500e75f6f480a7c
Harold Thetiot <hthetiot@gmail.com>
2017-06-28 19:00:06 -0700

    add basic time display to check component draw

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -11,6 +11,33 @@ exports.Main = Component.specialize(/** @lends Main# */ {
     constructor: {
         value: function Main() {
             this.super();
+            this.startTimer();
         }
+    },
+    _currentDate: {
+    	value: null,
+    },
+    currentDate: {
+    	set: function (date) {
+    		this._currentDate = date;
+    		this.needDraw = true;
+
+    		return date;
+    	},
+    	get: function () {
+    		return this._currentDate;
+    	}
+    },
+    _timer: {
+    	value: null,
+    },
+    startTimer: {
+    	value: function () {
+    		var self = this;
+    		clearInterval(this._timer);
+    		this._timer = setInterval(function () {
+    			self.currentDate = new Date();
+    		})
+    	}
     }
 });
